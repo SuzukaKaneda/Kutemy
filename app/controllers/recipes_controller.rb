7463e9d4ea61a3cf8ba3929857f8fd2e51b7d771
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-03-10 17:10:40 +0900

    add: Lint,デバッグ,レシピコントローラー

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,7 +25,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: "Recipe was successfully created." }
+        format.html { redirect_to @recipe, notice: "レシピの作成に成功しました。" }
         format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: "Recipe was successfully updated." }
+        format.html { redirect_to @recipe, notice: "レシピの更新に成功しました。" }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit, status: :unprocessable_entity }
