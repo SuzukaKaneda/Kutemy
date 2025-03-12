a3ec2ed4d55f612c42052075d5e5276b90e10a2b
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-03-12 22:47:38 +0900

    add: 画像アップロード機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,6 +65,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :trick, :get_point)
+      params.require(:recipe).permit(:title, :trick, :get_point, :image, :image_cache)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_10_071720) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_12_131442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +20,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_10_071720) do
     t.datetime "updated_at", null: false
     t.text "trick"
     t.integer "get_point", null: false
+    t.string "image"
   end
 end
