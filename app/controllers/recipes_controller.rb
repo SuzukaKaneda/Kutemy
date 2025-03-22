eb656033950d4220b4047ac687cf3a153e1f9008
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-03-22 19:10:20 +0900

    add: 手順保存機能　fix: showとformに手直し

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,7 +69,7 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :trick, :get_point, :image, :image_cache, ingredients_attributes: [:id, :name, :quantity, :_destroy])
+      params.require(:recipe).permit(:title, :trick, :get_point, :image, :image_cache, ingredients_attributes: [:id, :name, :quantity, :_destroy], instructions_attributes: [:id, :step, :content, :_destroy] )
     end
 
     def retouch_image(image)
