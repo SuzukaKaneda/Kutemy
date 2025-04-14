343db1fdf9b74365da138b55589888f1d5302bf3
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-04-14 16:12:02 +0900

    Fix: ログイン後のページ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
+
+  private 
+ def after_sign_in_path_for(resource)
+   recipes_look_path # ログイン後に遷移するpathを設定
+ end
+
+ def after_sign_out_path_for(resource)
+   recipes_look_path # ログアウト後に遷移するpathを設定
+ end
 end
