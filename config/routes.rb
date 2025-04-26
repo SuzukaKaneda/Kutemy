083bdf556472715c627e2ebbeee71442424851fe
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-04-26 19:23:49 +0900

    fix: rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
   resources :favorites, only: %i[create destroy]
-  
+
   root "others#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
