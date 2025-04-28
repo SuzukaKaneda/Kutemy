199b97d673472972dacec20b25606e967a39c03b
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-04-28 15:20:45 +0900

    add; コメント編集機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,37 @@
 class CommentsController < ApplicationController
-    def create
-        @comment = current_user.comments.build(comment_params)
-        @comment.save
-    end
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
     
-    def destroy
-        @comment = current_user.comments.find(params[:id])
-        @comment.destroy!
-    end
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @recipe = @comment.recipe
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @recipe = @comment.recipe
+    if @comment.update(comment_edit_params)
+        # 更新成功時の処理
+      else
+        # 更新失敗時の処理
+        puts @comment.errors.full_messages
+      end
+  end
 
     private
   
     def comment_params
       params.require(:comment).permit(:content).merge(recipe_id: params[:recipe_id])
+    end
+    
+    def comment_edit_params
+        params.require(:comment).permit(:content)
     end
   end
