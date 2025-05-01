e216aaad319b5c52f8f72574c1553e4bbdeef072
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-05-01 19:13:18 +0900

    add: メール送信環境

--- config/routes.rb
+++ config/routes.rb
@@ -35,4 +35,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
