9b1bb6a1b917a8a53690c4c4db2a1b42b01ee94e
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-05-04 14:00:42 +0900

    add: Google認証

--- Gemfile
+++ Gemfile
@@ -55,6 +55,10 @@ gem "high_voltage"
 gem "letter_opener_web"
 gem "dotenv-rails"
 
+# SNS認証
+gem "omniauth-google-oauth2"
+gem "omniauth-rails_csrf_protection"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_26_110736) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_03_122924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_26_110736) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "point", default: 0
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
