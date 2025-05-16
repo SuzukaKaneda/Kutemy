3a833770946ee735d3187c918b187df3d264e48b
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-05-16 21:37:24 +0900

    add: test.rbの追記

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,12 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Cloudinary.config do |config|
+    config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
+    config.api_key = Rails.application.credentials.cloudinary[:api_key]
+    config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
+    config.secure = true
+    config.cdn_subdomain = true
+  end
 end
