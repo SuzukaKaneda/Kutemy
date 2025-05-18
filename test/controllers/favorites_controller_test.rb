853113f1bd92e34dd5c1c69f4483cc6b233b07d8
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-05-18 16:31:24 +0900

    fix: gem

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -10,13 +10,13 @@ class FavoritesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create favorite" do
-    assert_difference('Favorite.count', 1) do
+    assert_difference("Favorite.count", 1) do
       post favorites_path, params: { recipe_id: @recipe2.id }
     end
   end
 
   test "should destroy favorite" do
-    assert_difference('Favorite.count', -1) do
+    assert_difference("Favorite.count", -1) do
       favorite = favorites(:one)  # テストデータから特定のFavoriteを取得
       delete favorite_path(favorite)  # 正しいIDを渡して削除リクエストを送る
     end
