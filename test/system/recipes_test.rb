4a73fca0bd9b0662afaa9ee48bde9c83f2139d6f
金田涼香 <kanedasuzuka@kanedaryoukanoMacBook-Air.local>
2025-05-18 17:24:15 +0900

    test内容3 rubcop

--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class RecipesTest < ApplicationSystemTestCase
   fixtures :users
   setup do
-    @user = users(:one) 
+    @user = users(:one)
     sign_in @user
     @recipe = recipes(:one)
   end
@@ -15,28 +15,27 @@ class RecipesTest < ApplicationSystemTestCase
 
   test "should create recipe" do
     visit recipes_url
-    find('.post').click
-    find('.post').click
-    find('input.title', visible: true).set(@recipe.title)
-    find('input.point', visible: true).set(@recipe.get_point)
-    click_button '投稿'
+    find(".post").click
+    find(".post").click
+    find("input.title", visible: true).set(@recipe.title)
+    find("input.point", visible: true).set(@recipe.get_point)
+    click_button "投稿"
     assert_text "レシピを作成しました。"
   end
 
   test "should update Recipe" do
     visit recipe_url(@recipe)
     click_on "編集"
-    find('input.title', visible: true).set(@recipe.title)
-    find('input.point', visible: true).set(@recipe.get_point)
-    click_on '投稿'
+    find("input.title", visible: true).set(@recipe.title)
+    find("input.point", visible: true).set(@recipe.get_point)
+    click_on "投稿"
     assert_text "レシピの更新に成功しました。"
   end
 
   test "should destroy Recipe" do
     visit recipe_url(@recipe)
     click_on "削除", match: :first
-    page.accept_alert '本当に削除しますか？' do
-      click_on "OK"
+    page.accept_alert "本当に削除しますか？" do
     end
     assert_text "レシピの削除に成功しました。"
   end
